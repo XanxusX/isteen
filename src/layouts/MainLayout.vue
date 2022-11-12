114a222a6010e914f711e4f9d03a3a44e8101bdc
XanxusX <tako30215@hotmail.com>
2022-11-12 16:07:39 +0800

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,160 @@
+<template>
+  <q-layout view="lHh Lpr lff">
+    <q-header style="background: transparent">
+      <q-toolbar>
+        <q-toolbar-title></q-toolbar-title>
+
+        <q-btn
+          flat
+          round
+          dense
+          icon="fa-brands fa-instagram"
+          herf="https://www.instagram.com/isteenlee/"
+          class="q-mr-sm"
+        />
+        <q-btn
+          flat
+          round
+          dense
+          icon="fa-brands fa-twitch"
+          href="https://www.twitch.tv/isteenlee"
+          class="q-mr-sm"
+        />
+        <q-btn
+          flat
+          round
+          dense
+          icon="fa-brands fa-youtube"
+          href="https://www.youtube.com/channel/UCQg1Dl3d5ixyH_NMB0p4kUg/featured"
+          class="q-mr-sm"
+        />
+        <q-btn
+          flat
+          round
+          dense
+          icon="fa-brands fa-tiktok"
+          href="https://www.tiktok.com/@isteenlee"
+          class="q-mr-sm"
+        />
+        <q-btn
+          flat
+          round
+          dense
+          icon="fa-brands fa-facebook"
+          href="https://www.facebook.com/isteenlee"
+          class="q-mr-sm"
+        />
+        <q-btn
+          flat
+          round
+          dense
+          icon="fa-brands fa-spotify"
+          href="https://open.spotify.com/artist/0oUpAcUVbIduFLEpZatuBc?si=RlxhhtnlTWa6EWcCjpLwIA"
+          class="q-mr-sm"
+        />
+        <q-btn
+          flat
+          round
+          dense
+          icon="fa-solid fa-music"
+          href="https://music.apple.com/tw/artist/%E6%9D%8E%E6%AC%A3%E5%BA%AD/1606950614"
+          class="q-mr-sm"
+        />
+        <q-btn
+          flat
+          round
+          dense
+          icon="fa-brands fa-discord"
+          href="https://discord.com/invite/DnJb494XYv"
+        />
+      </q-toolbar>
+    </q-header>
+    <q-img
+      src="https://scontent.ftpe8-1.fna.fbcdn.net/v/t39.30808-6/290676756_434258388709475_737166682712733880_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=uW6Uxct--G0AX_-0n7_&_nc_ht=scontent.ftpe8-1.fna&oh=00_AfBDtjEO0_nE9SVzzsybkrIuadz6lNg6fwxu2WLFK9HGOQ&oe=637052F7"
+      :height="CalHeight()"
+      style="position: fixed"
+      :ratio="16 / 9"
+      fit="cover"
+    ></q-img>
+    <q-footer class="bg-transparent" style="backdrop-filter: brightness(0.3)">
+      <q-toolbar>
+        <q-toolbar-title class="text-body2 text-center"
+          >Made with
+          <q-icon color="red" name="fa-solid fa-heart" class="q-mx-sm" /> by
+          <q-btn
+            flat
+            color="secondary"
+            label="Fan"
+            href="https://github.com/XanxusX/isteen"
+          />
+          所有素材版權皆屬於唯有音樂
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import EssentialLink, {
+  EssentialLinkProps,
+} from 'components/EssentialLink.vue';
+
+const essentialLinks: EssentialLinkProps[] = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev',
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework',
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev',
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev',
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev',
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev',
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev',
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+const CalHeight = () => {
+  return `${window.innerHeight}px`;
+};
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,106 @@
+<template>
+  <q-page class="items-center justify-evenly">
+    <div
+      class="row items-center justify-evenly heading"
+      :style="{ height: CalTopHeight() }"
+    >
+      <transition appear class="animated fadeInDown delay-1s slower">
+        <h2
+          class="text-white"
+          :style="{
+            'letter-spacing': '0.25em',
+            'margin-top': '20vh',
+            position: 'absolute',
+            'margin-right': '45vw',
+          }"
+        >
+          李欣庭 &nbsp;Isteen
+        </h2>
+      </transition>
+      <transition appear class="animated fadeInDown delay-1s slower">
+        <h4
+          class="text-white"
+          :style="{
+            'letter-spacing': '0.15em',
+            'margin-top': '50vh',
+            position: 'absolute',
+            'margin-left': '15vw',
+          }"
+        >
+          <span style="color: white; background: rgb(118, 185, 0)">NVIDIA</span>
+          創辦人暨執行長 黃仁勳認證的好聲音
+        </h4>
+      </transition>
+      <transition
+        appear
+        class="animated infinite pulse slow"
+        :style="{ 'margin-top': '75vh', position: 'absolute' }"
+      >
+        <q-btn
+          round
+          outline
+          color="white"
+          icon="keyboard_arrow_down"
+          href="#intro"
+        />
+      </transition>
+    </div>
+    <div
+      class="row items-center justify-evenly"
+      style="backdrop-filter: brightness(0.3)"
+      id="intro"
+    >
+      <div class="col-12 col-lg-6 items-center text-center q-py-xl q-px-lg">
+        <h6 class="text-white">
+          國立臺灣師範大學國文學系畢業，正職是音樂公司企劃，下班在 Twitch
+          兼差當歌手。
+          曾拿過流音之星全國高中職創作暨歌唱大賽獨唱組的亞軍、政大金旋獎獲得獨唱組冠軍，低沉的嗓音為招牌特色，歌聲中充滿感情，一開口就令人想哭，被
+          Twitch 觀眾戲稱「殯葬系歌手」。 2022/11/4與李以樂於 Wirforce
+          戶外直播巧遇 Nvidia 創辦人暨執行長 黃仁勳，現場即興點播《Always
+          Remember Us This Way》獲得一片好評。
+        </h6>
+        <q-video
+          :ratio="16 / 9"
+          src="https://www.youtube.com/embed/uL8KEzQ0J84"
+        ></q-video>
+      </div>
+      <div class="row items-center justify-evenly">
+        <div class="col-12 col-lg-6 items-center text-center q-py-xl q-px-lg">
+          <h5 class="text-white">最新單曲 寂寞星球</h5>
+          <p class="text-white">
+            小王子曾對飛行員說過：「當一個人好哀傷好哀傷的時候，就會喜歡看落日。」所以他在一天裡看了四十三次落日。
+            我喜歡落日，也喜歡落日後的滿天星辰。
+            或許每個人的心中都住著一顆寂寞星球，只有在世界沈靜時，才能閉起眼睛，看見點點星光。
+            你知道嗎？所有大人都曾經是小朋友，只是我們都忘記了。
+          </p>
+          <q-video
+            :ratio="16 / 9"
+            src="https://www.youtube.com/embed/4bpT-MlCRmY"
+          ></q-video>
+        </div>
+      </div>
+      <div class="row items-center justify-evenly">
+        <div class="col-12 col-lg-6 items-center text-center q-py-xl"></div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/ExampleComponent.vue';
+import { ref } from 'vue';
+
+const slide = ref(1);
+
+const meta = ref<Meta>({
+  totalCount: 1200,
+});
+const CalTopHeight = () => {
+  return `${window.innerHeight - 50}px`;
+};
+
+const CalSectionHeight = () => {
+  return `${window.innerHeight}px`;
+};
+</script>
